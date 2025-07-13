eb11132f418b50dc760e6f9fffb31b1c4b27efed
jackcat13 <christophehenry11@gmail.com>
2025-07-13 15:53:29 +0200

    feat(cursor): Add cursor at current position and refactor/fix insert mode for insertions/deletions over multiple lines

--- src/filesbuffers.rs
+++ src/filesbuffers.rs
@@ -1,31 +1,99 @@
 use std::collections::HashMap;
 
-pub type FileBuffer = Vec<char>;
+use ratatui::layout::Position;
 
 #[derive(Default, Debug, Clone)]
-pub struct FilesBuffers(HashMap<String, (FileBuffer, i32)>);
+pub struct FilesBuffers {
+    files: HashMap<String, FileBuffer>,
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct FileBuffer {
+    pub file: Vec<Vec<char>>,
+    pub current_line: u16,
+    pub current_column: u16,
+    pub lines_number: u16,
+}
 
 impl FilesBuffers {
     pub fn new() -> Self {
-        Self(HashMap::new())
+        FilesBuffers {
+            files: HashMap::new(),
+        }
     }
 
     pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
+        self.files.is_empty()
     }
 
     pub fn init_file_buffer(&mut self, key: String) {
-        self.0.insert(key, (vec![], 0));
+        self.files.insert(
+            key,
+            FileBuffer {
+                file: vec![vec![]],
+                current_line: 0,
+                current_column: 0,
+                lines_number: 0,
+            },
+        );
     }
 
-    pub fn get(&self, key: String) -> (FileBuffer, i32) {
-        self.0
+    pub fn get(&self, key: String) -> FileBuffer {
+        self.files
             .get(&key)
             .cloned()
             .expect("Failed to get file buffer")
     }
 
-    pub fn get_mut(&mut self, key: String) -> &mut (FileBuffer, i32) {
-        self.0.get_mut(&key).expect("Failed to get file buffer")
+    pub fn get_mut(&mut self, key: String) -> &mut FileBuffer {
+        self.files.get_mut(&key).expect("Failed to get file buffer")
+    }
+}
+
+impl FileBuffer {
+    pub fn get_mut(&mut self) -> &mut Vec<char> {
+        self.file
+            .get_mut(self.current_line as usize)
+            .expect("Failed to get file buffer for given line")
+    }
+
+    pub fn to_cursor_position(&self) -> Position {
+        Position {
+            x: self.current_column,
+            y: self.current_line,
+        }
+    }
+
+    pub fn create_line(&mut self) {
+        self.file.insert(self.current_line as usize + 1, vec![]);
+        self.current_line += 1;
+        self.current_column = 0;
+        self.lines_number += 1;
+    }
+
+    pub fn delete_line(&mut self) {
+        let current_line = self.current_line as usize;
+        self.file.remove(current_line);
+        self.current_line -= 1;
+        self.current_column = self.get_mut().len() as u16;
+        self.lines_number -= 1;
+    }
+
+    pub fn insert_char(&mut self, input: char) {
+        let current_column = self.current_column as usize;
+        let buffer = self.get_mut();
+        buffer.insert(current_column, input);
+        self.current_column += 1;
+    }
+
+    pub fn delete_previous_position(&mut self) {
+        if self.current_column > 0 {
+            let current_column = self.current_column as usize - 1;
+            let buffer = self.get_mut();
+            buffer.remove(current_column);
+            self.current_column -= 1;
+        } else if self.current_line > 0 {
+            self.delete_line();
+        }
     }
 }
